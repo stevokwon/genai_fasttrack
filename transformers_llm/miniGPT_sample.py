05c9260a03103c04b54dca27ec29aa8b0ae1da0c
stevokwon <130387986+stevokwon@users.noreply.github.com>
2025-04-26 03:58:57 +0000

    Created training loop and sampling logic

--- transformers_llm/miniGPT_sample.py
+++ transformers_llm/miniGPT_sample.py
@@ -41,3 +41,42 @@ n_embed = 32 # Embedding Size
 
 model = MiniGPT(vocab_size, n_embed)
 optimizer = torch.optim.AdamW(model.parameters(), lr = 1e-2)
+
+# Training Loop
+batch_size = 4
+block_size = 4 # Context Length
+
+for step in range(300):
+    # get random batch
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size]] for i in ix)
+    y = torch.stack([data[i+1:i+block_size]] for i in ix)
+
+    # Forward
+    logits = model(x)
+    logits = logits.view(-1, vocab_size)
+    y = y.view(-1)
+    loss = f.cross_entropy(logits, y)
+
+    # Backward
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if step % 50 == 0:
+        print(f"Step {step}, Loss : {loss.item():.4f}")
+
+# Start with context of 1 character
+context = torch.zeros((1, 1), dtype = torch.long)
+
+# Generate 100 characters
+generated = []
+for _ in range(100):
+    logits = model(context)
+    logits = logits[:, -1, :] # Take the last time step
+    probs = f.softmax(logits, dim = -1)
+    idx_next = torch.multinomial(probs, num_samples = 1)
+    generated.append(idx_next.item())
+    context = torch.cat((context, idx_next), dim = 1)
+
+print(decode(generated))
